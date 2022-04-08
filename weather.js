74e0d8e0a95734bb363adc0a1acdfcff6bd79a95
Nikita Dubyk <nikita_dubyk@mail.ru>
2022-04-08 17:39:43 +0300

    added log services

--- weather.js
+++ weather.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js'
+import { printError, printHelp, printSuccess } from './services/log.service.js'
 
 const initCLI = () => {
     const args = getArgs(process.argv)
-    console.log(args)
+    if (args.h) {
+        printHelp()
+    }
+    if (args.s) {
+    }
+    if (args.t) {
+    }
 }
 
 initCLI()
