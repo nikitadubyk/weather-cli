d12752556f540e7f10fde4cac2bf7d64ac19656e
Nikita Dubyk <nikita_dubyk@mail.ru>
2022-04-09 11:35:35 +0300

    added api service, storage

--- weather.js
+++ weather.js
@@ -1,6 +1,21 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js'
+import { getWeather } from './services/api.service.js'
 import { printError, printHelp, printSuccess } from './services/log.service.js'
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js'
+
+const saveToken = async token => {
+    if (!token.length) {
+        printError('Не передан токен')
+        return
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.token, token)
+        printSuccess('Токен сохранен')
+    } catch (error) {
+        printError(error.message)
+    }
+}
 
 const initCLI = () => {
     const args = getArgs(process.argv)
@@ -10,7 +25,9 @@ const initCLI = () => {
     if (args.s) {
     }
     if (args.t) {
+        return saveToken(args.t)
     }
+    getWeather('Горловка')
 }
 
 initCLI()
