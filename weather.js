443133fcff76849edc003c580d906c0495939c1f
Nikita Dubyk <nikita_dubyk@mail.ru>
2022-04-09 13:17:49 +0300

    finished the app

--- weather.js
+++ weather.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js'
-import { getWeather } from './services/api.service.js'
-import { printError, printHelp, printSuccess } from './services/log.service.js'
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js'
+import { getIcon, getWeather } from './services/api.service.js'
+import {
+    printError,
+    printHelp,
+    printSuccess,
+    printWeather,
+} from './services/log.service.js'
+import {
+    saveKeyValue,
+    TOKEN_DICTIONARY,
+    getKeyValue,
+} from './services/storage.service.js'
 
 const saveToken = async token => {
     if (!token.length) {
@@ -17,17 +26,50 @@ const saveToken = async token => {
     }
 }
 
+const saveCity = async city => {
+    if (!city.length) {
+        printError('Не передан город')
+        return
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.city, city)
+        printSuccess('Город сохранен')
+    } catch (error) {
+        printError(error.message)
+    }
+}
+
+const getForcast = async () => {
+    try {
+        const city =
+            process.env.CITY ?? (await getKeyValue(TOKEN_DICTIONARY.city))
+        const weather = await getWeather(city)
+        printWeather(weather, getIcon(weather.weather[0].icon))
+    } catch (error) {
+        if (error.response.status === 401) {
+            return printError('Неверно указан токен')
+        } else if (error.response.status === 404) {
+            return printError('Неверно указан город')
+        } else {
+            return printError(error.message)
+        }
+    }
+}
+
 const initCLI = () => {
     const args = getArgs(process.argv)
     if (args.h) {
         printHelp()
     }
     if (args.s) {
+        // сохранение города
+        console.log(args.s)
+        return saveCity(args.s)
     }
     if (args.t) {
         return saveToken(args.t)
     }
-    getWeather('Горловка')
+    getForcast()
 }
 
 initCLI()
